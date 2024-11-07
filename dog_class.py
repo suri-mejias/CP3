95c402f6d7ed2a3530593a88b855a245da172e84
suri-mejias <suri.mejias@ucas-edu.net>
2024-11-06 17:46:14 -0700

    finished dogs class

--- dog_class.py
+++ dog_class.py
@@ -2,5 +2,16 @@ class Dog:
     def __init__(self, name, breed):
         self.name = name
         self.breed = breed
+    def __str__(self):
+        return f"{self.name} is a {self.breed}"
 
-    dogs = ['Marceline': German Sheperd, 'Cajun': Belgian Malinois, 'Daisy': Border Collie ]
+dogs = [
+    Dog("Marceline", "German Sheperd"),
+    Dog("Cajun", "Belgian Malinois")
+    Dog("Daisy", "Border Collie")
+    Dog("Rocky", "Golden Retriever")
+    Dog("Bella", "Irish Setter")
+    ]
+
+for dog in dogs:
+    print(dog)
